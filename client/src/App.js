bb47bb510e65dff7e1bed015a6e84ea95f3207c4
Ollan Muza <128623143+kxngollan@users.noreply.github.com>
2023-09-10 15:55:35 +0100

    fetch data, userInput add

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import { useCallback, useState } from 'react';
 import './App.css';
+import UserInput from './components/UserInputs/UserInput';
 
 function App() {
+  const [games, setGames] = useState([]);
+  const [error, setError] = useState(null);
+
+  const FetchGameData = useCallback(async () => {
+    try {
+      const response = await fetch('http://localhost:8000/api');
+      if (!response.ok) {
+        throw new Error('Something went wrong when recieving data');
+      }
+      const data = await response.json();
+
+      const teamGameDays = Object.keys(data).map((key) => ({
+        id: data[key].id,
+        opponent: data[key].opponent,
+        gameDay: data[key].gameDay,
+        venue: data[key].venue,
+        time: data[key].time,
+        teamScore: data[key].teamScore,
+        oppScore: data[key].oppScore,
+        gameStatus: data[key].gameStatus,
+      }));
+
+      const sortedGames = [...teamGameDays].sort((game1, game2) => {
+        new Date(game1.date) - new Date(game2.date);
+      });
+
+      setGames(sortedGames);
+    } catch (error) {
+      setError(error.message);
+    }
+  });
+
+  const onGameSubmission = (game) => {};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserInput onAddGameDay={onGameSubmission} />
     </div>
   );
 }
---
+++ client/src/components/UserInputs/UserInput.js
@@ -0,0 +1,6 @@
+import GameDetails from './GameDetails.Js';
+
+const UserInput = (props) => {
+  return <GameDetails onAddGame />;
+};
+export default UserInput;
